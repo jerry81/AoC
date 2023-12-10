aa0b120e787d01c3cd75468ed479ffd046e4c23c
Raskolnikov <jerry.tann@endress.com>
2023-12-10 14:13:32 +0800

    🚧 core

--- day9/mirageMaintenence.cpp
+++ day9/mirageMaintenence.cpp
@@ -10,17 +10,36 @@ using namespace std;
 const string FNAME = "sminput.txt";
 
 int main() {
-  long long int sum = 0;
+  long long int sm = 0;
   ifstream strm(FNAME);
   string line;
 
   while (getline(strm, line)) {
+    vector<vector<int>> stk;
     vector<int> items;
-    cout << "line is " << line << endl;
     istringstream spaceStream(line);
     string spaceToken;
     while (spaceStream >> spaceToken) {
       items.push_back(stoi(spaceToken));
+    }
+    stk.push_back(items);
+    bool all_zeros = false;
+    while (!all_zeros) {
+      vector<int> cur_line;
+      all_zeros = true;
+      vector<int> analyzed = stk.back();
+      for (int i = 0; i < analyzed.size()-1;++i) {
+        int it1=analyzed[i];
+        int it2=analyzed[i+1];
+        int diff = it2 - it1;
+        cur_line.push_back(diff);
+        if (diff != 0) all_zeros = false;
+      }
+      cout << "pushing " << endl;
+      for (int c: cur_line) {
+        cout << c << endl;
+      }
+      stk.push_back(cur_line);
     }
   }
   cout << "sum " << sm << endl;
