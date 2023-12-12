d5d5a43f2e3fd02f2c67527e137511b7b2c15c70
Raskolnikov <jerry.tann@endress.com>
2023-12-12 12:14:01 +0800

    🚧 attempt 1

--- 2023/day11/cosmicExpansion.cpp
+++ 2023/day11/cosmicExpansion.cpp
@@ -2,10 +2,20 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
-const string FNAME = "input.txt";
+const string FNAME = "sm.txt";
+
+string to_h(int y, int x) {
+  return to_string(y) + "," + to_string(x);
+}
+
+pair<int, int> to_pos(string s) {
+  auto pos = s.find(',');
+  return {stoi(s.substr(0, pos)), stoi(s.substr(pos + 1))};
+}
 
 vector<string> read_lines_into_vec() {
   ifstream strm(FNAME);
@@ -23,6 +33,7 @@ vector<string> expand_galaxy(vector<string> orig, int h, int w) {
     full_line.push_back('.');
   }
   vector<string> new_galaxy;
+
   for (int i = 0; i < orig.size(); ++i) {
     new_galaxy.push_back(orig[i]);
     if (orig[i].find('#') == string::npos) new_galaxy.push_back(full_line);
@@ -39,8 +50,11 @@ vector<string> expand_galaxy(vector<string> orig, int h, int w) {
     if (!found) empty_cols.push_back(col);
   }
   for (int c: empty_cols) {
-    cout << c << " is empty " << endl;
+    for (int r = 0; r < new_galaxy.size(); ++r) {
+      new_galaxy[r].insert(c, ".");
+    }
   }
+  return new_galaxy;
 }
 
 int main () {
@@ -48,5 +62,31 @@ int main () {
   int h = lines.size();
   int w = lines[0].size();
   vector<string> expanded = expand_galaxy(lines,h,w);
+
+  vector<pair<int,int>> items;
+  for (int r = 0; r < expanded.size(); ++r) {
+    string row = expanded[r];
+    for (int c = 0; c < row.size(); ++c) {
+      if (row[c] == '#') {
+        items.push_back({r,c});
+      }
+    }
+  }
+  int totalDists = 0;
+  int count = 0;
+  for (int i = 0; i < items.size()-1; ++i) {
+    pair<int,int> point1 = items[i];
+    auto [y1,x1] = point1;
+    for (int j = i+1; j < items.size(); ++j) {
+      pair<int,int> point2 = items[j];
+      auto [y2,x2] = point2;
+      count++;
+      int dist = abs(y1-y2) + abs(x1-x2);
+      cout << "dist is " << dist << endl;
+      totalDists += dist;
+    }
+  }
+  cout << "count is " << count << endl;
+  cout << "totalDist is " << totalDists << endl;
   return 0;
 }
