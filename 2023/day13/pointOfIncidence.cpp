d4c0d1b566304e76e444e131474de121a94c6e55
Raskolnikov <jerry.tann@endress.com>
2023-12-13 15:06:42 +0800

    🚧 example handled

--- 2023/day13/pointOfIncidence.cpp
+++ 2023/day13/pointOfIncidence.cpp
@@ -17,6 +17,57 @@ vector<string> read_lines_into_vec() {
   return lines;
 }
 
+long long int horizontalCheck(vector<string> lines) {
+  vector<int> nextcandidates;
+  for (auto b : lines) {
+    // line of a grid
+    int len = b.size();
+    if (nextcandidates.empty()) {
+      for (int i = 0; i < len - 1; ++i) {
+        char c = b[i];
+        char d = b[i + 1];
+        if (c == d) {
+          nextcandidates.push_back(i);
+        }
+      }
+    } else {
+      vector<int> nextnextcandds;
+      for (int i : nextcandidates) {
+        if (b[i] == b[i + 1]) nextnextcandds.push_back(i);
+      }
+      nextcandidates = nextnextcandds;
+    }
+  }
+   if (nextcandidates.size() == 1) return nextcandidates[0];
+    return -1;
+}
+
+long long int verticalCheck(vector<string> lines) {
+  vector<int> nextcandidates;
+  int ht = lines.size();
+  // line of a grid
+  int wd = lines[0].size();
+  for (int i = 0; i < wd; ++i) {
+    if (nextcandidates.empty()) {
+      for (int j = 0; j < ht - 1; ++j) {
+        if (lines[j][i] == lines[j+1][i]) {
+          nextcandidates.push_back(j);
+        }
+      }
+    } else {
+      vector<int> nxtnxt;
+      for (int j: nextcandidates) {
+        if (lines[j][i] == lines[j+1][i]) nxtnxt.push_back(j);
+
+        nextcandidates = nxtnxt;
+      }
+    }
+  }
+  if (nextcandidates.size() == 1) return nextcandidates[0];
+  return -1;
+}
+
+
 int main() {
   vector<string> all_lines = read_lines_into_vec();
   vector<vector<string>> grids;
@@ -30,27 +81,16 @@ int main() {
     }
   }
   grids.push_back(cur_grid);
+  long long int res;
   for (auto a : grids) {
-    vector<int> nextcandidates;
-    for (auto b : a) {
-      // line of a grid
-      int len = b.size();
-      if (nextcandidates.empty()) {
-        for (int i = 0; i < len - 1; ++i) {
-          char c = b[i];
-          char d = b[i + 1];
-          if (c == d) {
-            nextcandidates.push_back(i);
-          }
-        }
-      } else {
-        vector<int> nextnextcandds;
-        for (int i: nextcandidates) {
-          if (b[i] == b[i+1]) nextnextcandds.push_back(i);
-        }
-      }
-      if (nextcandidates.size() == 1) cout << "split point found at " << nextcandidates[0] << endl;
-    }
+    long long int sum = 0;
+    int hres = horizontalCheck(a);
+    if (hres >=0) sum+= 100*hres;
+    int vres = verticalCheck(a);
+    if (vres >= 0) sum+= vres;
+    cout << "sum is " << sum << endl;
+    res+= sum;
   }
+  cout << "res is " << res << endl;
   return 0;
 }
