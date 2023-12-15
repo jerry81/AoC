5db759c15fe861872414660615911bfabb1bb536
Raskolnikov <jerry.tann@endress.com>
2023-12-15 14:16:12 +0800

    🚧

--- 2023/day15/part2.cpp
+++ 2023/day15/part2.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-const string FNAME = "input.txt";
+const string FNAME = "sm.txt";
 
 vector<string> read_lines_into_vec() {
   ifstream strm(FNAME);
@@ -34,17 +34,35 @@ int hsh(char c, int init) {
   return init % 256;
 }
 
+int hsh_s(string s) {
+  int cur = 0;
+  for (char c: s) {
+    cur = hsh(c,cur);
+  }
+  return cur;
+}
+
 int main() {
   string s = read_lines_into_vec()[0];
   vector<string> tks = split_by(s,',');
   int sum = 0;
   for (auto tk: tks) {
-    int cur = 0;
-    for (char c: tk) {
-      cur = hsh(c, cur);
+    string k;
+    string v;
+    if (tk.find('-') != string::npos) {
+      k = split_by(tk, '-')[0];
+      int h = hsh_s(k);
+      cout << "removing " << k << " from position " << h << endl;
+    } else {
+      auto ve = split_by(tk, '=');
+      k = ve[0];
+      v = ve[1];
+      int h = hsh_s(k);
+      cout << "eq situation " << endl;
+      cout << "assinging " << stoi(v) <<  " into position " << h << "," << k << endl;
     }
-    sum+=cur;
   }
+  vector<vector<tuple<string, int>>> slots(256);
   cout << sum << endl;
   return 0;
 }
