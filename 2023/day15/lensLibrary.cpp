d3cbd422c479c30243e2eba330748754c7ac6c33
Raskolnikov <jerry.tann@endress.com>
2023-12-15 13:50:09 +0800

    🚧 att 1

--- 2023/day15/lensLibrary.cpp
+++ 2023/day15/lensLibrary.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-const string FNAME = "sm.txt";
+const string FNAME = "input.txt";
 
 vector<string> read_lines_into_vec() {
   ifstream strm(FNAME);
@@ -28,5 +28,23 @@ vector<string> split_by(string s, char c) {
   return newTokens;
 }
 
+int hsh(char c, int init) {
+  init+=c;
+  init *= 17;
+  return init % 256;
+}
+
 int main() {
+  string s = read_lines_into_vec()[0];
+  vector<string> tks = split_by(s,',');
+  int sum = 0;
+  for (auto tk: tks) {
+    int cur = 0;
+    for (char c: tk) {
+      cur = hsh(c, cur);
+    }
+    sum+=cur;
+  }
+  cout << sum << endl;
+  return 0;
 }
