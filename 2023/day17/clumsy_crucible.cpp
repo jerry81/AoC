715c1985506e5fd61a7dd06c93cafbf6e8cff33b
Raskolnikov <jerry.tann@endress.com>
2023-12-19 15:59:25 +0800

    🚧

--- 2023/day17/clumsy_crucible.cpp
+++ 2023/day17/clumsy_crucible.cpp
@@ -21,6 +21,7 @@ vector<string> read_lines_into_vec() {
 // DIR: 0L 1U 2R 3D
 int dfs(int y, int x, int dir, int consecutive, set<pair<int, int>> visited,
         vector<string> &grid, int &h, int &w) {
+  int cur_val = grid[y][x] - '0';
   visited.insert({y, x});
   if (y == h - 1 && x == w - 1) return grid[y][x];
 
@@ -55,14 +56,44 @@ int dfs(int y, int x, int dir, int consecutive, set<pair<int, int>> visited,
       nxt_dirs.push_back(2);
     }
   }
+
+  int res = INT_MAX;
+  for (int nd: nxt_dirs) {
+    int ny = y;
+    int nx = x;
+    switch (nd) {
+      case 0: {
+        nx--;
+        break;
+      }
+      case 1: {
+        ny--;
+        break;
+      }
+      case 2: {
+        nx++;
+        break;
+      }
+      default: {
+        ny++;
+      }
+    }
+    if (ny < 0 || nx < 0) continue;
+
+    if (ny >= h || nx > w) continue;
+
+    if (visited.find({ny,nx}) != visited.end()) continue;
+
+    int cons = nd == dir ? consecutive+1 : 0;
+    res = min(res, dfs(ny,nx,nd, cons, visited, grid, h, w));
+  }
+  return res;
 }
 
 int main() {
   vector<string> grid = read_lines_into_vec();
   int h = grid.size();
   int w = grid[0].size();
-  cout << "grid" << endl;
-  for (string s : grid) cout << s << endl;
   // dfs?
   return 0;
 }
