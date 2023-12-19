fb2b4a2d3d9a9a42b9cd33a3e1471628696c3928
Raskolnikov <jerry.tann@endress.com>
2023-12-19 12:26:42 +0800

    🚧

--- 2023/day16/floorIsLava.cpp
+++ 2023/day16/floorIsLava.cpp
@@ -1,8 +1,8 @@
 #include <fstream>
-#include <string>
-#include <vector>
 #include <iostream>
 #include <set>
+#include <string>
+#include <vector>
 
 using namespace std;
 
@@ -18,22 +18,88 @@ vector<string> read_lines_into_vec() {
   return lines;
 }
 
-int main () {
-  set<tuple<int,int,int>> visited; // directions 0 L 1 U 2 R 3 D
+int main() {
+  set<tuple<int, int, int>> visited;  // directions 0 L 1 U 2 R 3 D
   vector<string> lines = read_lines_into_vec();
   int h = lines.size();
   int w = lines[0].size();
-  vector<tuple<int,int,int>> current_beams;
-  auto start = make_tuple(0,0,2);
+  vector<tuple<int, int, int>> current_beams;
+  auto start = make_tuple(0, 0, 2);
   current_beams.push_back(start);
   while (!current_beams.empty()) {
-    vector<tuple<int,int,int>> nxt_beams;
-    for (auto [y,x,dir]: current_beams) {
-      if (visited.find({y,x,dir}) == visited.end()) continue;
+    vector<tuple<int, int, int>> nxt_beams;
+    for (auto [y, x, dir] : current_beams) {
+      if (visited.find({y, x, dir}) == visited.end()) continue;
 
-      visited.insert({y,x,dir});
+      visited.insert({y, x, dir});
+      char cur = lines[y][x];
       int nx = x;
       int ny = y;
+      vector<int> nxt_dirs;
+      switch (cur) {
+        case '.': {
+          nxt_dirs.push_back(dir);
+          break;
+        }
+        case '\\': {
+          switch (dir) {
+            case 0: {
+              nxt_dirs.push_back(1);
+              break;
+            }
+            case 1: {
+              nxt_dirs.push_back(0);
+              break;
+            }
+            case 2: {
+              nxt_dirs.push_back(3);
+              break;
+            }
+            default: {
+              nxt_dirs.push_back(2);
+            }
+          }
+          break;
+        }
+        case '/': {
+          switch (dir) {
+            case 0: {
+              nxt_dirs.push_back(3);
+              break;
+            }
+            case 1: {
+              nxt_dirs.push_back(2);
+              break;
+            }
+            case 2: {
+              nxt_dirs.push_back(1);
+              break;
+            }
+            default: {
+              nxt_dirs.push_back(0);
+            }
+          }
+          break;
+        }
+        case '-': {
+          if (dir == 1 || dir == 3) {
+            nxt_dirs.push_back(0);
+            nxt_dirs.push_back(2);
+          } else {
+            nxt_dirs.push_back(dir);
+          }
+          break;
+        }
+        default: {  // |
+          if (dir == 0 || dir == 2) {
+            nxt_dirs.push_back(1);
+            nxt_dirs.push_back(3);
+          } else {
+            nxt_dirs.push_back(dir);
+          }
+          break;
+        }
+      }
       switch (dir) {
         case 0: {
           nx--;
@@ -52,6 +118,7 @@ int main () {
         }
       }
       if (nx < 0 || ny < 0) continue;
+
       if (ny >= h || nx >= w) continue;
     }
   }
