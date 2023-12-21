8d87b21d44c2c2077e592cc87d9d6b9875d7fa0c
Raskolnikov <jerry.tann@endress.com>
2023-12-21 14:58:59 +0800

    🚧

--- 2023/day19/aplenty.cpp
+++ 2023/day19/aplenty.cpp
@@ -30,27 +30,48 @@ vector<string> split_by(string s, char c) {
   return newTokens;
 }
 
+bool run(vector<int> props) {
+}
+
 int main () {
   vector<string> all_lines = read_lines_into_vec();
-  vector<string> props;
+  vector<vector<int>> props; // decompose further 0x1m2a3s
   unordered_map<string, vector<string>> workflows;
   string s = ".";
   int i = 0;
   while (true) {
     s = all_lines[i];
     if (s.empty()) break;
+
     int pos = s.find('{');
     string key = s.substr(0, pos);
     string remainder = s.substr(pos+1,s.size()-2-pos);
-
     vector<string> rules = split_by(remainder, ',');
     workflows[key] = rules;
     ++i;
   }
   ++i;
   for (int j = i; j < all_lines.size(); ++j) {
-    props.push_back(all_lines[j]);
+    string str = all_lines[j];
+    string trimmed = str.substr(1,str.size()-2);
+    vector<string> tokens = split_by(trimmed, ',');
+    vector<int> itokens;
+    for (string token:tokens) {
+      vector<string> spltokens = split_by(token, '=');
+      itokens.push_back(stoi(spltokens[1]));
+    }
+    props.push_back(itokens);
   }
+  long long int res = 0;
+
+  for (vector<int> v: props) {
+    long long int sum = 0;
+    for (int i: v) sum+=i;
+
+    if (run(v)) res+=sum;
+    // go through workflow - if pass then add to res
+  }
+  cout << "res is " << res << endl;
   return 0;
 }
 
