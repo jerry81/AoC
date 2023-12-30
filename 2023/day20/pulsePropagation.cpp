8259a1c8f8a6722d714c363510d6220257282068
Raskolnikov <jerry.tann@endress.com>
2023-12-30 09:10:44 +0800

    ✨

---
+++ 2023/day20/pulsePropagation.cpp
@@ -0,0 +1,50 @@
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+const string FNAME = "sm.txt";
+
+vector<string> read_lines_into_vec() {
+  ifstream strm(FNAME);
+  string line;
+  vector<string> lines;
+  while (getline(strm, line)) {
+    lines.push_back(line);
+  }
+  return lines;
+}
+
+
+vector<string> split_by_str(string s, string delim) {
+  size_t pos = 0;
+  string token1;  // define a string variable
+  vector<string> res;
+  while ((pos = s.find(delim)) != std::string::npos) {
+    token1 = s.substr(0, pos);  // store the substring
+    res.push_back(token1);
+    s.erase(0, pos + delim.length()); /* erase() function store the current
+                                         positon and move to next token. */
+  }
+  return res;
+}
+
+struct Signal {
+  char type;
+  vector<string> outputs;
+};
+
+int main() {
+  vector<string> signals = read_lines_into_vec();
+  unordered_map<string, Signal> signals_map;
+  for (string s: signals) {
+    vector<string> tokens = split_by_str(s, "->");
+    cout << "tokens " << endl;
+    for (string s2: tokens) cout << s2 << endl;
+  }
+  return 0;
+}
---
+++ util/splitByStr.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+using namespace std;
+vector<string> split_by_str(string s, string delim) {
+  size_t pos = 0;
+  string token1;  // define a string variable
+  vector<string> res;
+  while ((pos = s.find(delim)) != std::string::npos) {
+    token1 = s.substr(0, pos);  // store the substring
+    res.push_back(token1);
+    s.erase(0, pos + delim.length()); /* erase() function store the current
+                                         positon and move to next token. */
+  }
+  return res;
+}
