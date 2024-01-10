c5a909de0168422e54e645897d0a24cdfe6a8f06
Raskolnikov <jerry.tann@endress.com>
2024-01-10 14:05:00 +0800

    ✨ just start it

---
+++ 2023/day25/snoverload.cpp
@@ -0,0 +1,88 @@
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+const string FNAME = "sm.txt";
+
+vector<string> read_lines_into_vec() {
+  ifstream strm(FNAME);
+  string line;
+  vector<string> lines;
+  while (getline(strm, line)) {
+    lines.push_back(line);
+  }
+  return lines;
+}
+
+vector<string> split_by_str(string s, string delim) {
+  size_t pos = 0;
+  string token1;  // define a string variable
+  vector<string> res;
+  while ((pos = s.find(delim)) != std::string::npos) {
+    token1 = s.substr(0, pos);  // store the substring
+    res.push_back(token1);
+    s.erase(0, pos + delim.length()); /* erase() function store the current                                positon and move to next token. */
+  }
+  res.push_back(s);
+  return res;
+}
+
+int main() {
+  // brute force: create disjoint set omitting 012 013 014... 2^n possibilities
+  vector<string> lines = read_lines_into_vec();
+  for (string s: lines) {
+    vector<string> tokens = split_by_str(s,": ");
+    string parent = tokens[0];
+    vector<string> neighbors = split_by_str(tokens[1], " ");
+    cout << parent << "'s neighbors are " << endl;
+    for (string t: neighbors) cout << t << endl;
+  }
+  return 0;
+}
+
+/*
+
+jqt: 4 <-
+rhn: 4
+xhk: 5
+nvd: 5 <-
+rsh: 4
+frs: 4
+pzl: 4 <-
+lsr: 5
+hfx: 5 <-
+cmg: 5 <-
+qnr: 4
+lhk: 4
+bvb: 5 <-
+ntq: 4
+rzs: 4
+
+doesn't seem to have anything to do with connection count.
+
+jqt: rhn xhk nvd
+rsh: frs pzl lsr
+xhk: hfx
+cmg: qnr nvd lhk bvb
+rhn: xhk bvb hfx
+bvb: xhk hfx
+pzl: lsr hfx nvd
+qnr: nvd
+ntq: jqt hfx bvb xhk
+nvd: lhk
+lsr: lhk
+rzs: qnr cmg lsr rsh
+frs: qnr lhk lsr
+
+exactly 3 wires to cut that make 2 disjoint sets.
+
+TIL: this is called global minimum cut algorithm
+classic problem
+- network design, social network, etc
+- karger is popular
+  - randomized algo
+  - repeatedly "contract" random edges until only 2 nodes
+*/
--- util/splitByStr.cpp
+++ util/splitByStr.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 using namespace std;
+
 vector<string> split_by_str(string s, string delim) {
   size_t pos = 0;
   string token1;  // define a string variable
@@ -8,8 +9,8 @@ vector<string> split_by_str(string s, string delim) {
   while ((pos = s.find(delim)) != std::string::npos) {
     token1 = s.substr(0, pos);  // store the substring
     res.push_back(token1);
-    s.erase(0, pos + delim.length()); /* erase() function store the current
-                                         positon and move to next token. */
+    s.erase(0, pos + delim.length()); /* erase() function store the current                                positon and move to next token. */
   }
+  res.push_back(s);
   return res;
 }
