4054bf5fe3864c5b564c385b3b152c3f4e3b965f
Raskolnikov <jerry.tann@endress.com>
2024-01-11 11:51:46 +0800

    🚧 so far not bad

--- 2023/day25/snoverload2.cpp
+++ 2023/day25/snoverload2.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <iostream>
 #include <iterator>
+#include <queue>
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
@@ -35,39 +36,40 @@ vector<string> split_by_str(string s, string delim) {
   return res;
 }
 
-auto bfs = [&](const auto &edges, string from,
-               string to) {  // automatically deduce type (lambda function) [&]
-                             // capture clause (capture variables by reference )
-  unordered_set<string> visited;
-  queue<vector<string>> paths;
-  visited.insert(from);
-  paths.push({from});
-
-  while (!paths.empty()) {
-    auto path =
-        move(paths.front());  // std library, move operation - ref to front
-                              // element, provide access to it, treat as rvalue
-    paths.pop();
-    if (path.back() == to) return path;
-
-    for (auto edge_to : edges[path.back()]) {
-      if (visited.contains(edge_to))
-        continue  // instead of find
-
-            visited.insert(edge_to);
-      auto next = path;
-      next.push_back(edge_to);
-      paths.push(
-          move(next));  // move is an optimizer.  still not sure when to use it.
-    }
-  }
-  return {};
-};
-
 int main() {
   // brute force: create disjoint set omitting 012 013 014... 2^n possibilities
   unordered_map<string, unordered_set<string>> e;
   srand((unsigned)time(0));
+
+  auto bfs = [&](const auto &edges, string from,
+                 string
+                     to) {  // automatically deduce type (lambda function) [&]
+                            // capture clause (capture variables by reference )
+    unordered_set<string> visited;
+    queue<vector<string>> paths;
+    visited.insert(from);
+    paths.push({from});
+
+    while (!paths.empty()) {
+      auto path = move(
+          paths.front());  // std library, move operation - ref to front
+                           // element, provide access to it, treat as rvalue
+      paths.pop();
+      if (path.back() == to) return path;
+
+      for (auto edge_to : edges[path.back()]) {
+        if (visited.find(edge_to) != visited.end())
+          continue;  // instead of find, but only c++ 20
+
+        visited.insert(edge_to);
+        auto next = path;
+        next.push_back(edge_to);
+        paths.push(move(
+            next));  // move is an optimizer.  still not sure when to use it.
+      }
+    }
+    return vector<string>({});
+  };
 
   vector<string> lines = read_lines_into_vec();
   for (string s : lines) {
@@ -79,6 +81,10 @@ int main() {
       e[n].insert(parent);
     }
   }
+
+  auto s = begin(e)->first;
+  auto s2 = begin(e)->second.begin();
+  cout << "s is " << s << "and s2 is " << *s2 << endl;
 
   return 0;
 }
